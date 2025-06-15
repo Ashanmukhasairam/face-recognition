300c5df08dd0e6a756f8e20a20ae0f950f3fa458
Ashanmukhasairam <shanmukhaagnihotram@gmail.com>
2025-06-15 12:10:57 +0530

    13th deploy

---
+++ backend/__init__.py
@@ -0,0 +1,3 @@
+"""
+Face Recognition Attendance System Backend Package
+"""
--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="face-recognition-app",
+    packages=["backend"],
+    include_package_data=True,
+    package_data={
+        "backend": [
+            "templates/*.html",
+            "*.xlsx",
+            "data/*",
+            "known_faces/*"
+        ]
+    }
+)
